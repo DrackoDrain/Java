1e5b19f474f323f38e2dd7478f5dd9c615ecc810
Ayush <ayush0999aryan@gmail.com>
2023-11-16 18:48:57 +0530

    set the file in order

--- JAva/src/Java_Programs/HelloWorld.java
+++ JAva/src/Java_Programs/HelloWorld.java
@@ -1,7 +1,8 @@
-public class HEl11 {
+package Java_Programs;
+
+public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello I am Ayush Aryan 1 ");
 
     }
-
 }
--- JAva/src/NEw_Practice.java
+++
@@ -1,6 +0,0 @@
-public class NEw_Practice {
-
-    public static void main(String[] args){
-        System.out.println("HEllo i am AYush Aryan 2");
-    }
-}
